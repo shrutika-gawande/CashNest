f37c3be98213b8b4ec82a41d7ffdb8c99d388497
shrutika-gawande <shrutikagawande80@gmail.com>
2026-05-19 13:10:46 +0530

    edit context

--- frontend/src/context/ModalContext.jsx
+++ frontend/src/context/ModalContext.jsx
@@ -4,19 +4,22 @@ const ModalContext = createContext();
 
 export function ModalProvider({children}) {
     const [modal, setModal] = useState(false);
+    const [editItem, setEditItem] = useState(null);
     const [type, setType] = useState("");
 
-    const openModal = (type) => {
+    const openModal = (type, item = null) => {
         setType(type);
+        setEditItem(item);  // null = add mode, item = edit mode
         setModal(true);
     }
 
     const closeModal = () => {
         setModal(false);
+        setEditItem(null); // clear on close
     }
 
     return (
-        <ModalContext.Provider value={{modal, type, openModal, closeModal}}>
+        <ModalContext.Provider value={{editItem, modal, type, openModal, closeModal}}>
             {children}
         </ModalContext.Provider>
     );
