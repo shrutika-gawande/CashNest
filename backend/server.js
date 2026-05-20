864d7ebeda1eb4cbe13f6dcfb6e4f60e8741d677
shrutika-gawande <shrutikagawande80@gmail.com>
2026-05-20 21:20:28 +0530

    logic to get seed data

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-import incomeRoutes from "./routes/incomeRoutes.js"
-import expenseRoutes from "./routes/expenseRoutes.js"
-import budgetRoutes from "./routes/budgetRoutes.js"
+import incomeRoutes from "./routes/incomeRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
+import budgetRoutes from "./routes/budgetRoutes.js";
+import seedData from "./seed.js";
+import Income from "./models/Income.js";
+import Expense from "./models/Expense.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +16,23 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+const seedDatabase = async () => {
+  const incomeCount = await Income.countDocuments();
+  const expenseCount = await Expense.countDocuments();
+
+  if (incomeCount === 0) {
+    await Income.insertMany(seedData.incomes);
+    console.log("Seed incomes added");
+  }
+
+  if (expenseCount === 0) {
+    await Expense.insertMany(seedData.expenses);
+    console.log("Seed expenses added");
+  }
+};
+
+seedDatabase();
 
 app.use("/api/incomes", incomeRoutes);
 app.use("/api/expenses", expenseRoutes);
